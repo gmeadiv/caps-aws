44a61571dfe4caf970eafb15872790443310d8f4
George Mead <gmeadiv@gmail.com>
2021-11-11 17:35:38 -0800

    attempts debug

--- sns-consumer.js
+++ sns-consumer.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Consumer } = require('sqs-consumer');
+const {Consumer} = require('sqs-consumer');
 
 const queueUrl = 'https://sqs.us-east-1.amazonaws.com/567736933253/meadg-message-queue';
 
